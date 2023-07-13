ce0ea9385dd821de2e164e9f1f3edafb71c8352f
Denis_L <denis88lyapin@gmail.com>
2023-07-14 00:17:02 +0300

    added  add_foreign_keys to postgre_db.py

--- homework-5/postgre_db.py
+++ homework-5/postgre_db.py
@@ -49,13 +49,14 @@ class PostgresDB:
                     contact_name varchar(30) NOT NULL,
                     contact_title varchar(30) NOT NULL,
                     address varchar(60),
-                    citi varchar(60),
+                    citi varchar(60) NOT NULL,
                     region varchar(30),
                     postal_code varchar(30),
-                    country varchar(30),
-                    phone varchar(30),
+                    country varchar(30) NOT NULL,
+                    phone varchar(30) NOT NULL,
                     fax varchar(30),
-                    homepage varchar(100))
+                    homepage varchar(100),
+                    products text)
                     """
                     )
             print("Таблица suppliers успешно создана")
@@ -75,14 +76,14 @@ class PostgresDB:
                 'company_name': supplier.get('company_name'),
                 'contact_name': contact[0].strip(),
                 'contact_title': contact[1].strip(),
-                'address': address[4].strip(),
+                'address': address[4].strip() if address[4].strip() else None,
                 'citi': address[3].strip(),
-                'region': address[1].strip(),
-                'postal_code': address[2].strip(),
+                'region': address[1].strip() if address[1].strip() else None,
+                'postal_code': address[2].strip() if address[2].strip() else None,
                 'country': address[0].strip(),
-                'phone': supplier.get('phone'),
-                'fax': supplier.get('fax'),
-                'homepage': supplier.get('homepage'),
+                'phone': supplier.get('phone').strip(),
+                'fax': supplier.get('fax') if supplier.get('fax') else None,
+                'homepage': supplier.get('homepage').strip() if supplier.get('homepage').strip() else None,
                 'products': supplier.get('products')
             }
             suppliers.append(supplier_tmp)
@@ -92,19 +93,44 @@ class PostgresDB:
         """Добавляет данные из suppliers в таблицу suppliers."""
         try:
             for supplier in suppliers:
+
                 self.cur.execute(
                     '''
-                    INSERT INTO suppliers (company_name, contact_name, contact_title, address,
-                                           citi, region, postal_code, country, phone, fax, homepage)
-                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+                    INSERT INTO suppliers (company_name,   contact_name, contact_title, address,
+                                           citi,           region,       postal_code,   country,
+                                           phone,          fax,          homepage,      products)
+                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
                                          
                     ''',
                     (supplier.get('company_name'), supplier.get('contact_name'), supplier.get('contact_title'),
                      supplier.get('address'),      supplier.get('citi'),         supplier.get('region'),
                      supplier.get('postal_code'),  supplier.get('country'),      supplier.get('phone'),
-                     supplier.get('fax'), supplier.get('homepage'))
+                     supplier.get('fax'),          supplier.get('homepage'),     supplier.get('products'))
                 )
             print('Данные в таблицу suppliers успешно добавлены')
+        except(Exception, psycopg2.DatabaseError) as error:
+            print(error)
+
+    def add_foreign_keys(self) -> None:
+        """Добавляет foreign key со ссылкой на supplier_id в таблицу products."""
+        try:
+            # self.cur.execute('''ALTER TABLE products DROP COLUMN supplier_id''')
+            self.cur.execute('''
+                                ALTER TABLE products ADD COLUMN supplier_id INTEGER;
+                                UPDATE products
+                                SET supplier_id = suppliers.supplier_id
+                                FROM suppliers
+                                WHERE EXISTS (
+                                  SELECT 1
+                                  FROM unnest(suppliers.products::text[]) AS sp(product_name)
+                                  WHERE products.product_name = sp.product_name
+                                  );
+                                
+                                ALTER TABLE products ADD CONSTRAINT fk_products_suppliers FOREIGN KEY(supplier_id)
+                                REFERENCES suppliers(supplier_id)
+                            ''')
+            self.cur.execute('''ALTER TABLE suppliers DROP COLUMN products;''')
+            print('foreign key в таблицу products успешно добавлен')
         except(Exception, psycopg2.DatabaseError) as error:
             print(error)
 
@@ -119,10 +145,12 @@ if __name__ == '__main__':
     db = PostgresDB(params)
     db.create_database('my_new_db')
     db.execute_sql_script("fill_db.sql")
+    # # print(db.conn)
     db.create_suppliers_table()
     d = db.get_suppliers_data('suppliers.json')
     db.insert_suppliers_data(d)
-    db.close_connection()
-    print(d)
 
+    db.add_foreign_keys()
+
+    db.close_connection()
 
